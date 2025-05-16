d0be596bd7a7070909c07499c56a7162ad2ebe69
Vannamma N A <83291550+Vannamma148@users.noreply.github.com>
2025-05-16 13:25:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Slide from './Slide.jsx'
+import Look from './Look.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
         <Slide/>
-
+  <Look/>
   </StrictMode>,
 )
